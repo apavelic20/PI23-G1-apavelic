725baa1b24d0211db3b0557665135b26919f0f50
Aduel13 <adrianpavelic@gmail.com>
2023-05-07 22:43:23 +0200

    Izvršeno programiranje prijave - klikom na gumb Prijava u formi obavijestit će korisnika da li je zaboravio upisati lozinku, da li je zaboravio upisati korisničko ime, da li su podaci kirvi uneseni ili je sve ispravno uneseno

--- EvaluationManager/FrmLogin.Designer.cs
+++ EvaluationManager/FrmLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EvaluationManager
 {
-    partial class Form1
+    partial class FrmLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -38,9 +38,10 @@
             // lblKorIme
             // 
             this.lblKorIme.AutoSize = true;
-            this.lblKorIme.Location = new System.Drawing.Point(12, 31);
+            this.lblKorIme.Location = new System.Drawing.Point(16, 38);
+            this.lblKorIme.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblKorIme.Name = "lblKorIme";
-            this.lblKorIme.Size = new System.Drawing.Size(75, 13);
+            this.lblKorIme.Size = new System.Drawing.Size(94, 16);
             this.lblKorIme.TabIndex = 0;
             this.lblKorIme.Text = "Korisničko ime";
             this.lblKorIme.Click += new System.EventHandler(this.label1_Click);
@@ -48,49 +49,61 @@
             // lblLozinka
             // 
             this.lblLozinka.AutoSize = true;
-            this.lblLozinka.Location = new System.Drawing.Point(12, 79);
+            this.lblLozinka.Location = new System.Drawing.Point(16, 97);
+            this.lblLozinka.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLozinka.Name = "lblLozinka";
-            this.lblLozinka.Size = new System.Drawing.Size(44, 13);
+            this.lblLozinka.Size = new System.Drawing.Size(53, 16);
             this.lblLozinka.TabIndex = 1;
             this.lblLozinka.Text = "Lozinka";
             this.lblLozinka.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtIme
             // 
-            this.txtIme.Location = new System.Drawing.Point(125, 28);
+            this.txtIme.Location = new System.Drawing.Point(167, 34);
+            this.txtIme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIme.Name = "txtIme";
-            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.Size = new System.Drawing.Size(132, 22);
             this.txtIme.TabIndex = 2;
+            this.txtIme.TextChanged += new System.EventHandler(this.txtIme_TextChanged);
             // 
             // txtLozinka
             // 
-            this.txtLozinka.Location = new System.Drawing.Point(125, 76);
+            this.txtLozinka.Location = new System.Drawing.Point(167, 94);
+            this.txtLozinka.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtLozinka.Name = "txtLozinka";
-            this.txtLozinka.Size = new System.Drawing.Size(100, 20);
+            this.txtLozinka.Size = new System.Drawing.Size(132, 22);
             this.txtLozinka.TabIndex = 3;
+            this.txtLozinka.UseSystemPasswordChar = true;
             this.txtLozinka.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // btnPrijava
             // 
-            this.btnPrijava.Location = new System.Drawing.Point(158, 168);
+            this.btnPrijava.Location = new System.Drawing.Point(211, 207);
+            this.btnPrijava.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrijava.Name = "btnPrijava";
-            this.btnPrijava.Size = new System.Drawing.Size(75, 23);
+            this.btnPrijava.Size = new System.Drawing.Size(100, 28);
             this.btnPrijava.TabIndex = 4;
             this.btnPrijava.Text = "Prijava";
             this.btnPrijava.UseVisualStyleBackColor = true;
+            this.btnPrijava.Click += new System.EventHandler(this.btnPrijava_Click);
             // 
-            // Form1
+            // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.ClientSize = new System.Drawing.Size(379, 260);
             this.Controls.Add(this.btnPrijava);
             this.Controls.Add(this.txtLozinka);
             this.Controls.Add(this.txtIme);
             this.Controls.Add(this.lblLozinka);
             this.Controls.Add(this.lblKorIme);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prijava";
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ EvaluationManager/FrmLogin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManager
+{
+    public partial class FrmLogin : Form
+    {
+        string username = "nastavnik";
+        string password = "test";
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+            if (txtIme.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Problem",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtLozinka.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!", "Problem", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (txtIme.Text == username && txtLozinka.Text == password)
+                {
+                    MessageBox.Show("Dobrodošli!", "Prijavljeni ste",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Krivi podaci!", "Problem", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void txtIme_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
